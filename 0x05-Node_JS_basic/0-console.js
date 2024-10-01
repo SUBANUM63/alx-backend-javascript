5e640efb7dabdbe6f62eaed5ff0bf055178cc4a2
Zeinu Kedir <zeinuka641@gmail.com>
2024-10-01 07:36:16 +0000

    NodeJS Basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author  Zeinu Kedir <https://github.com/SUBANUM63>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
